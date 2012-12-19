8a8bc31cfae9eb84a9c42f6273d6eb2dbcb8da9a
gamehaxe@gmail.com <gamehaxe@gmail.com@4033e1c8-57e6-11de-bc7c-c9df4fa8b3c6>
2012-12-19 15:43:27 +0000

    Update linux binaries for sdl 1.2.15

--- trunk/include/ogg/config_types.h
+++ trunk/include/ogg/config_types.h
@@ -1,6 +1,7 @@
 #ifndef __CONFIG_TYPES_H__
 #define __CONFIG_TYPES_H__
 
+
 #ifdef BLACKBERRY
 
     /* QNX */
@@ -12,12 +13,13 @@
    typedef int64_t ogg_int64_t;
    
 #else
+#  include <inttypes.h>
 
 /* these are filled in by configure */
 typedef int16_t ogg_int16_t;
-typedef u_int16_t ogg_uint16_t;
+typedef unsigned short ogg_uint16_t;
 typedef int32_t ogg_int32_t;
-typedef u_int32_t ogg_uint32_t;
+typedef unsigned int ogg_uint32_t;
 typedef int64_t ogg_int64_t;
 
 #endif
